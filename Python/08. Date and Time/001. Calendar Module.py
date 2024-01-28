368eee50c5469ae39fca67e45fcc0c9d5c6b0763
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2024-01-28 04:18:25 -0800

    Delete Python/08. Date and Time directory

--- Python/08. Date and Time/001. Calendar Module.py
+++
@@ -1,9 +0,0 @@
-# Problem: https://www.hackerrank.com/challenges/calendar-module/problem
-# Score: 10
-
-
-import calendar
-
-
-m, d, y = map(int, input().split())
-print(calendar.day_name[calendar.weekday(y, m, d)].upper())
