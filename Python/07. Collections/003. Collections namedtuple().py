b112376d7ca52e6b1a213b4dd86141fc48864639
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2024-01-28 04:18:08 -0800

    Delete Python/07. Collections directory

--- Python/07. Collections/003. Collections namedtuple().py
+++
@@ -1,9 +0,0 @@
-# Problem: https://www.hackerrank.com/challenges/py-collections-namedtuple/problem
-# Score: 20
-
-
-from collections import namedtuple
-
-
-n, Student = int(input()), namedtuple('Student', input())
-print("{:.2f}".format(sum([int(Student(*input().split()).MARKS) for _ in range(n)]) / n))
