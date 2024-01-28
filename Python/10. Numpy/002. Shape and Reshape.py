ec6eb527c7c187010723f32a8b0c5fa2a31d72f2
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2024-01-28 04:18:56 -0800

    Delete Python/10. Numpy directory

--- Python/10. Numpy/002. Shape and Reshape.py
+++
@@ -1,9 +0,0 @@
-# Problem: https://www.hackerrank.com/challenges/np-shape-reshape/problem
-# Score: 20
-
-
-import numpy as np
-
-
-array = np.array(list(map(int, input().split())))
-print(array.reshape(3, 3))
